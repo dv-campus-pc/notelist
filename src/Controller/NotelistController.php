ee0f0a13ba0b5b7a02a0acfb5299f62623446f90
{DV.Campus} <dv.campus.pc@gmail.com>
2021-10-26 21:05:29 +0300

    Added create/delete note controller

--- src/Controller/NotelistController.php
+++ src/Controller/NotelistController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Note;
+use App\Repository\NoteRepository;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class NotelistController extends AbstractController
 {
+    private NoteRepository $noteRepository;
+
+    public function __construct(NoteRepository $noteRepository)
+    {
+        $this->noteRepository = $noteRepository;
+    }
+
     private array $categories = [
         1 => [
             'title' => 'My Summer weekends',
@@ -140,6 +149,42 @@ class NotelistController extends AbstractController
 
         return $this->render('notelist/get.html.twig', [
             'note' => $note
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function createAction(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $newNote = new Note();
+        $newNote
+            ->setTitle('New note title')
+            ->setText('New note text');
+
+        $entityManager->persist($newNote);
+        $entityManager->flush();
+
+        $notes = $this->noteRepository->findAll();
+
+        return $this->render('notelist/list.html.twig', [
+            'notes' => $notes,
+        ]);
+    }
+
+    /**
+     * @Route("/delete_note/{id}", name="create")
+     */
+    public function deleteAction(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $noteToDelete = $this->noteRepository->find($id);
+        $entityManager->remove($noteToDelete);
+        $entityManager->flush();
+
+        return $this->render('notelist/delete_note.html.twig', [
+            'id' => $id,
         ]);
     }
 }
