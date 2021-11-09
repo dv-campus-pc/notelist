b7167cbd22fd37c0265cd55c26a515c58755544d
{DV.Campus} <dv.campus.pc@gmail.com>
2021-11-09 19:26:52 +0200

    Lesson 6 - Entity relations: Implemented Category entity

---
+++ migrations/Version20211109170325.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20211109170325 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE category');
+    }
+}
