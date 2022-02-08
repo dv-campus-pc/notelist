2b6832059a233003c99e0497cb18357e2198d8bb
{DV.Campus} <dv.campus.pc@gmail.com>
2022-02-08 20:19:01 +0200

    Lesson 18 - Forms.Advanced: Change note form according to conditions: admin or owner

--- src/Form/NoteType.php
+++ src/Form/NoteType.php
@@ -3,14 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Note;
+use App\Entity\User;
+use App\Enum\RolesEnum;
 use App\Repository\CategoryRepository;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Event\PreSetDataEvent;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class NoteType extends AbstractType
 {
@@ -37,6 +39,11 @@ class NoteType extends AbstractType
                 'label' => 'Shared to users',
             ])
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            [$this, 'onPreSetData']
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -53,8 +60,26 @@ class NoteType extends AbstractType
         ]);
     }
 
-    private function getUser(): ?UserInterface
+    private function getUser(): ?User
     {
         return $this->tokenStorage->getToken() ? $this->tokenStorage->getToken()->getUser() : null;
+    }
+
+    public function onPreSetData(PreSetDataEvent $event)
+    {
+        $form =  $event->getForm();
+
+        if (!$this->getUser()->hasRole(RolesEnum::ADMIN)) {
+            $form->remove('users');
+        }
+
+        /** @var Note $data */
+        if (!$data = $event->getData()) {
+            return;
+        }
+
+        if ($data->getOwner() !== $this->getUser()) {
+            $form->remove('category');
+        }
     }
 }
