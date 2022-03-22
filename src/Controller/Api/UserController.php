3b2530cc5eadfa2a731dfcbd83fd4fbaff64dccc
Akinfiyev Sergey <akinfiyev.sergey@gmail.com>
2022-03-22 19:58:17 +0200

    Lesson 20 - API Implementation: Implemented Users list action

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -4,33 +4,45 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Model\API\ApiResponse;
 use App\Service\UserService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/user", name="user_")
  */
-class UserController extends AbstractController
+class UserController extends AbstractApiController
 {
     /**
      * @Route("/registration", name="registration", methods={"POST"})
      *
      * @IsGranted("IS_ANONYMOUS_USER")
      */
-    public function registration(Request $request, UserService $userService, SerializerInterface $serializer): Response {
-        $requestContent = $serializer->decode($request->getContent(), 'json');
+    public function registration(Request $request, UserService $userService): Response {
+        $requestContent = $this->serializer->decode($request->getContent(), 'json');
         $user = $userService->createAndFlush(
             (string) $requestContent['password'] ?? '',
             (string) $requestContent['username'] ?? ''
         );
 
-        return new Response($serializer->serialize($user, 'json', [
+        return new Response($this->serializer->serialize($user, 'json', [
             'groups' => ['API']
+        ]));
+    }
+
+    /**
+     * @Route("/list", name="list", methods={"GET"})
+     *
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function list(UserService $userService): Response {
+        $users = $userService->getUserList();
+
+        return new ApiResponse($this->serializer->serialize($users, 'json', [
+            'groups' => ['API'],
         ]));
     }
 }
